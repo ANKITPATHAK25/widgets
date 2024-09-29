0621ca2f6b109a9a3c4def174d4c1b0871ed792f
Ankit Pathak <104846348+ANKITPATHAK25@users.noreply.github.com>
2024-09-29 20:14:34 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Widget from './Widget';  // Import the Widget component
+
+function App() {
+  return (
+    <div className="App">
+      <Widget />  {/* Use the Widget component */}
+    </div>
+  );
+}
+
+export default App;
